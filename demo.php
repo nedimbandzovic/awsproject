b190244e6636f37995a2cbeee6a9f3980baa225a
Nedim Bandžović <nedim.bandzovic2001@gmail.com>
2022-03-22 18:41:56 +0100

    Small code refactor

--- demo.php
+++ demo.php
@@ -8,7 +8,7 @@
      'text' => 'Hi there',
      'to' => '38761648664',
      'api_key' => 'f68f2ebe',
-     'api_secret' => 'l5B7VZ1xVqWDlHKy',
+     'api_secret' => 'l5B7VZ1xVqWDlHKy'
 
 
  );
@@ -17,22 +17,30 @@
 
  // Prepare new cURL resource
  $crl = curl_init('https://rest.nexmo.com/sms/json');
- curl_setopt($crl, CURLOPT_RETURNTRANtSFER, true);
+ curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
  curl_setopt($crl, CURLINFO_HEADER_OUT, true);
  curl_setopt($crl, CURLOPT_POST, true);
  curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
 
  // Set HTTP Header for POST request
  curl_setopt($crl, CURLOPT_HTTPHEADER, array(
-     'Content-Type: application/json',
-     //'Content-Length: ' . strlen($payload))
+     'Content-Type: application/json'
+
+   )
  );
 
  // Submit the POST request
  $result = curl_exec($crl);
- print($result);
 
+ // handle curl error
+ if ($result === false) {
+     // throw new Exception('Curl error: ' . curl_error($crl));
+     //print_r('Curl error: ' . curl_error($crl));
+     $result_noti = 0; die();
+ } else {
 
+     $result_noti = 1; die();
+ }
  // Close cURL session handle
  curl_close($crl);
 
